960031db1097b39458aa22cfa54f0886950a08d5
mgland2021 <117710669+mgland2021@users.noreply.github.com>
2022-11-08 16:19:10 +0800

    delete test contract

--- scripts/deployer/spaceshipDeployer.ts
+++ scripts/deployer/spaceshipDeployer.ts
@@ -2,7 +2,7 @@ const {ethers} = require('hardhat');
 
 (async ()=> {
     const MGSpaceship = await ethers.getContractFactory("MGSpaceship");
-    const mgspaceship = await MGSpaceship.deploy("test122", "test2222");
+    const mgspaceship = await MGSpaceship.deploy("", "");
 
     await mgspaceship.deployed();
     console.log("Spaceship address is", mgspaceship.address);
--- scripts/deployer/spsmintDeployer.ts
+++ scripts/deployer/spsmintDeployer.ts
@@ -1,6 +1,6 @@
 (async ()=> {
     const SpaceshipMint = await ethers.getContractFactory("SpaceshipMint");
-    const mint = await SpaceshipMint.deploy("0x6bf2FF9c85014d60F01D918C376Ea2BDB6c7b218");
+    const mint = await SpaceshipMint.deploy("");
 
     await mint.deployed();
     console.log("mint address is", mint.address);
